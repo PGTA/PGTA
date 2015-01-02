6638ed49150cf040524c0c53e09164fdf8116895
Jordan Cristiano <jordan.cristi@gmail.com>
2015-01-02 18:22:15 -0500

    Added optional macro AK_USE_PGTA_C to allow C api use only

--- src/PGTA/akPGTA.h
+++ src/PGTA/akPGTA.h
@@ -2,7 +2,11 @@
 #ifndef AK_PGTA
 #define AK_PGTA
 
-#ifdef __cplusplus
+#if !defined(AK_USE_PGTA_C) && !defined(__cplusplus)
+#   define AK_USE_PGTA_C
+#endif
+
+#ifndef AK_USE_PGTA_C
 #include "public/akPGTAcpp.h"
 #else
 #include "public/akPGTAc.h"
