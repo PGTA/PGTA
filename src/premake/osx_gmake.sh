19a6660f07ccee2cd2f28a780e3018350115d33d
Keefer Davies <keeferdavies@rogers.com>
2015-02-21 16:22:40 -0500

    Fixing build script aesthetics.

--- src/premake/osx_gmake.sh
+++ src/premake/osx_gmake.sh
@@ -2,10 +2,10 @@
 
 runCommand()
 {
-	echo $1 && 	$1
-	if [ "$?" != "0" ]; then
-		echo "ERROR : $1" && exit 1
-	fi
+    echo $1 && $1
+    if [ "$?" != "0" ]; then
+        echo "ERROR : $1" && exit 1
+    fi
 }
 
 runCommand './../external/build-tools/bin/premake/premake5_osx clean --file=PGTA.lua'
