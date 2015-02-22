4ca978d815cacbab6c9d54745f438429513a91d7
Jordan Cristiano <jordan.cristi@gmail.com>
2015-02-22 00:25:14 -0500

    Removed unused build code

--- src/premake/AudioMixer.lua
+++ src/premake/AudioMixer.lua
@@ -2,8 +2,6 @@
 project "AudioMixer"
     kind "StaticLib"
     includedirs "../AudioMixer/include/"
-    --defines "PGTA_EXPORTS"
-    --dependson "FlatbufCompiler"
     files
     {
         "../AudioMixer/**.h",
